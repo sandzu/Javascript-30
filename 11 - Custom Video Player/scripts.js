07a86a04421d26a90913e4fd1be83b983c71a7c2
Andzu Schaefer <countermatter7@gmail.com>
2018-08-22 15:15:44 -0700

    worked more on day 11

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,11 @@
+(()=>{
+  const player = document.querySelector(".player");
+   video = document.querySelector(".player__video"),
+   progress = document.querySelector(".progress"),
+   progress/bar = document.querySelector(".progress__filled"),
+   toggle = player.querySelector(".toggle"),
+   skipButtons = player.querySelectorAll("[data-skip]"),
+   ranges = player.querySelectorAll(".player__slider");
+
+  // console.log(skip);
+})();
