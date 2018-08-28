099841759903aa86b241044c70e9571ade658d34
Andzu Schaefer <countermatter7@gmail.com>
2018-08-27 18:12:59 -0700

    work more on day 11

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,15 +2,15 @@
   const player = document.querySelector(".player");
    video = document.querySelector(".player__video"),
    progress = document.querySelector(".progress"),
-   progress/bar = document.querySelector(".progress__filled"),
+   progressBar = document.querySelector(".progress__filled"),
    toggle = player.querySelector(".toggle"),
    skipButtons = player.querySelectorAll("[data-skip]"),
    ranges = player.querySelectorAll(".player__slider");
 
-   function togglePlay() {
-     const method = video.paused ? 'play' : 'pause';
-     video[method]();
-   }
+   const
+    togglePlay = () => video[video.pasued? "play" : "pause"]();
+    updateButton = () => toggle.textContent = video.paused ?  "►" : "❚ ❚",
+    handleProgress = () => progressBar.style.flexBasis = `${video.currentTime / video.duration * 100}%`,
 
   // console.log(skip);
 })();
